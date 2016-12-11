43841060fc2cb52ca949de98210f9e37b1e56e2f
vineet <vineet@VINEET-PC>
2016-12-11 18:41:33 +1100

    second version

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -10,7 +10,7 @@ namespace GitTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello one world");
         }
     }
 }
